852bee27793cf9fe9087f75ff4a833bfd14709b1
davearich <dave@daverich.net>
2016-10-31 00:42:08 -0400

    Added Material Design look to navbar.

--- js/controls.js
+++ js/controls.js
@@ -1,42 +1,19 @@
 // This file is for JS used for interacting with control elements
 
 // Experience Blocks
-// TODO: make a single, reusable event handler
-$("#exp_expedient_btn").click(function(){
+$("#exp-more").click(function() {
 
-    if ($("#exp_expedient").css("display") == "none") {
-        $("#exp_expedient_btn").html('<span class="glyphicon glyphicon-menu-up"></span>&nbsp;&nbsp;Less');
+    if ($("#exp_adi").css("display") == "none") {
+        $("#exp-more").html('Less information');
     } else {
-        $("#exp_expedient_btn").html('<span class="glyphicon glyphicon-menu-down"></span>&nbsp;&nbsp;More');
+        $("#exp-more").html('More information');
     }
 
     // Expand the control
     $("#exp_expedient").slideToggle("fast");
-
-});
-
-$("#exp_staples_btn").click(function() {
-
-    if ($("#exp_staples").css("display") == "none") {
-        $("#exp_staples_btn").html('<span class="glyphicon glyphicon-menu-up"></span>&nbsp;&nbsp;Less');
-    } else {
-        $("#exp_staples_btn").html('<span class="glyphicon glyphicon-menu-down"></span>&nbsp;&nbsp;More');
-    }
-
-    // Expand the control
     $("#exp_staples").slideToggle("fast");
-});
-
-$("#exp_adi_btn").click(function() {
-
-    if ($("#exp_adi").css("display") == "none") {
-        $("#exp_adi_btn").html('<span class="glyphicon glyphicon-menu-up"></span>&nbsp;&nbsp;Less');
-    } else {
-        $("#exp_adi_btn").html('<span class="glyphicon glyphicon-menu-down"></span>&nbsp;&nbsp;More');
-    }
-
-    // Expand the control
     $("#exp_adi").slideToggle("fast");
+
 });
 
 
